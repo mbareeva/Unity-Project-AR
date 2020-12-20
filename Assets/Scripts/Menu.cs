61227ec9f068be23333eefb467460cc4e3d3ece9
mbareeva <beeemaja@gmail.com>
2020-12-20 21:36:18 +0100

    Main menu and paused menu added. Paused menu functional.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Transform canvas;
+    private bool isClicked = false;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    public void activatePause() {
+        Time.timeScale = 0;
+        canvas.gameObject.SetActive(true);
+    }
+
+    public void disactivatePause() {
+        Time.timeScale = 1;
+        canvas.gameObject.SetActive(false);
+        isClicked = false;
+    }
+
+    public void restart () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadMenu() {
+        SceneManager.LoadScene("Hauptmenu");
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            isClicked = !isClicked;
+        }
+
+        if(isClicked) {
+            activatePause();
+        } else {
+            disactivatePause();
+        }
+    }
+}
+
