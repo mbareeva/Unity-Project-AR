c7167b8ca14231cb8fb02367bc4dc444d1545be4
mbareeva <beeemaja@gmail.com>
2021-01-03 00:44:42 +0100

    Kick, punch, forward and backward movement of character. Gui for 2nd room. animation included.

---
+++ Assets/fighterController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fighterController : MonoBehaviour {
+    // Start is called before the first frame update
+    public Transform enemyTarget;
+    static Animator anim;
+    //buttons on the display.
+    public static bool mvBack = false;
+    public static bool mvFwd = false;
+    //make the script available for other sections/scripts.
+    public static fighterController instance;
+    public static bool isAttacking = false;
+    void Awake () {
+        if (instance == null) {
+            instance = this;
+        }
+    }
+    void Start () {
+        anim = GetComponent<Animator> ();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //make the moving back & fwd functional after kick or punch.
+    if(anim.GetCurrentAnimatorStateInfo(0).IsName("fight_idle")) {
+        isAttacking = false;
+    }
+
+        if (isAttacking == false) {
+            if (mvBack == true) {
+                anim.SetTrigger ("wkBack");
+                anim.ResetTrigger ("idle");
+            } else {
+                anim.SetTrigger ("idle");
+                anim.ResetTrigger ("wkBack");
+            }
+
+            if (mvFwd == true) {
+                anim.SetTrigger ("wkFwd");
+                anim.ResetTrigger ("idle");
+            } else if (mvBack == false) {
+                anim.SetTrigger ("idle");
+                anim.ResetTrigger ("wkFwd");
+            }
+        }
+
+    }
+
+    public void punch () {
+        isAttacking = true;
+        anim.ResetTrigger("idle");
+        anim.SetTrigger("punch");
+    }
+    public void kick () {
+        isAttacking = true;
+        anim.ResetTrigger("idle");
+        anim.SetTrigger("kick");
+    }
+    public void react () {
+        isAttacking = true;
+        anim.ResetTrigger("idle");
+        anim.SetTrigger("react");
+    }
+
+}
---
+++ Assets/walkFwdController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+// Controller for right (forward) button.
+public class walkFwdController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    // If the button is pressed, then it will change the boolean in the Animator's parameters.
+    public void OnPointerDown (PointerEventData data) {
+        fighterController.mvFwd = true;
+    }
+
+    public void OnPointerUp (PointerEventData data) {
+        fighterController.mvFwd = false;
+    }
+}
