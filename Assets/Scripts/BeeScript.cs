fa86ad2bf8ca801fb5c211b24eab5ae2f27acba3
alinachroma <alinachroma@gmail.com>
2021-01-11 20:21:52 +0100

    Revert of revert

--- Assets/Scripts/BeeScript.cs
+++ Assets/Scripts/BeeScript.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class BeeScript : MonoBehaviour
 {
+    public Transform arCamera;
 
     // Update is called once per frame (60 times pro sec)
     void Update()
     {
-        //make bees fly - moves the objects in the upper direction
-        transform.Translate(Vector3.up * Time.deltaTime * 0.2f);
+        arCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+      
+        //make bees fly - moves the objects towards the camera
+        transform.LookAt(arCamera, Vector3.up);
+        transform.Rotate(0, -100, 0);
+        transform.position = Vector3.Lerp(transform.position, arCamera.position, 0.01f);
+
+        //collision
+        float dist = Vector3.Distance(transform.position, arCamera.transform.position);
+        if (dist < 1)
+        {
+            Scores.scoreValue -= 1;
+            Destroy(gameObject);
+        }
         
     }
+
 }
