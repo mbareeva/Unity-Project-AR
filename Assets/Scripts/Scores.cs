b15153c941e5104ba28023efedd77ec7209eac25
alinachroma <alinachroma@gmail.com>
2021-01-12 18:32:28 +0100

    Scored edited

--- Assets/Scripts/Scores.cs
+++ Assets/Scripts/Scores.cs
@@ -18,6 +18,6 @@ public class Scores : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        score.text = "SCORE: " + scoreValue;
+        score.text = "X " + scoreValue;
     }
 }
