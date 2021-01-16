0faaf30879d427c56f1ffe0636407ab4501fcba7
mbareeva <beeemaja@gmail.com>
2021-01-17 00:53:55 +0100

    uebergang menu added.

---
+++ Assets/Scripts/Continue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Continue : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    public void goToFight() {
+        SceneManager.LoadScene("Spielraum_2");
+    }
+
+    public void QuitFight() {
+        Application.Quit();
+    }
+}
+
---
+++ Assets/Scripts/Uebergang.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Uebergang : MonoBehaviour
+{
+//Link: https://www.youtube.com/watch?v=1qbjmb_1hV4
+//Typewriter effect tutorial.
+    public float delay = 0.1f;
+    public string fullText;
+    private string currentText = "";
+    TextMeshPro text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(showText());
+    }
+    IEnumerator showText() {
+        for(int i = 0; i < fullText.Length; i++) {
+            currentText = fullText.Substring(0, i);
+            this.GetComponent<Text>().text =  currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
