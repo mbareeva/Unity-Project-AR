e59e42b40a3b096ebb5b0ea994f37550e9d5f2cf
alinachroma <alinachroma@gmail.com>
2021-01-19 19:11:35 +0100

    HealthBar in progress

---
+++ Assets/Scripts/ARCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ARCamera : MonoBehaviour
+{
+
+	public int maxHealth = 100;
+	public static int currentHealth;
+	public HealthBar healthBar;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))    
+		{
+			healthBar.SetMaxHealth(maxHealth);
+			currentHealth -= 20;
+		}
+	}
+
+	//void TakeDamage(int damage)
+	//{
+	//	currentHealth -= damage;
+
+	//	healthBar.SetHealth(currentHealth);
+	//}
+}
