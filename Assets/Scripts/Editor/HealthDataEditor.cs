0cd08920f1f505bfa35f44aef6fc159b11934755
alinachroma <alinachroma@gmail.com>
2021-01-23 16:39:37 +0100

    added HealthDataEditor

---
+++ Assets/Scripts/Editor/HealthDataEditor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+[CustomEditor(typeof(HealthData))]
+public class HealthDataEditor : Editor
+{
+    private HealthData targetObject;
+
+    void OnEnable()
+    {
+        targetObject = (HealthData)this.target;
+    }
+    
+    // Implement this function to make a custom inspector.
+    public override void OnInspectorGUI()
+    {
+        // Using Begin/End ChangeCheck is a good practice to avoid changing assets on disk that weren't edited.
+        EditorGUI.BeginChangeCheck();
+
+        //// Use the editor auto-layout system to make your life easy
+        EditorGUILayout.BeginVertical();
+
+        // Re-enable further controls
+        GUI.enabled = true;
+        targetObject.healthValue = EditorGUILayout.IntField("healthValue", targetObject.healthValue);
+        EditorGUILayout.EndVertical();
+
+        // If anything has changed, mark the object dirty so it's saved to disk
+        if (EditorGUI.EndChangeCheck())
+            EditorUtility.SetDirty(target);
+
+        if (targetObject.healthValue == 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
--- Assets/Scripts/ScriptableObjects/HealthData.cs
+++ Assets/Scripts/ScriptableObjects/HealthData.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [CreateAssetMenu]
 public class HealthData : ScriptableObject
 {
     public int healthValue;
-
+    
     public void OnEnable()
     {
         healthValue = 100;
     }
+    //
 }
